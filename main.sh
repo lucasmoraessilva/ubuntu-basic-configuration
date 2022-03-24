016ffcb129a9530aafbd447dd41d8594f5ee7b96
lucasmoraessilva <lucasmoraessilva01@gmail.com>
2022-03-24 08:32:02 -0300

    Criação do script main.sh que contém a execução principal para a configuração do ambiente

---
+++ main.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Author: Lucas Moraes da Silva
+# Creation date: 24/03/2022
+# E-mail: lucasmoraessilva01@gmail.com
+# Description: This script performs the minimum configuration required for a working environment on an Ubuntu distribution.
+# Use example: ./main.sh
+
+# Imports
+# ---------------------------------
+
+# Variables
+# ---------------------------------
+
+# Functions
+# ---------------------------------
+
+# Execution
+# ---------------------------------
